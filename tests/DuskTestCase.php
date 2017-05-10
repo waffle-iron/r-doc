242a87335ac8b5c9151bd749e9472801e70912d2
Robert Hamel <roberthamel@gmail.com>
2017-05-10 18:54:29 -0500

    fix attempt...

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,34 +2,37 @@
 
 namespace Tests;
 
+use App;
 use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 
 abstract class DuskTestCase extends BaseTestCase
 {
-    use CreatesApplication;
+  use CreatesApplication;
 
-    /**
-     * Prepare for Dusk test execution.
-     *
-     * @beforeClass
-     * @return void
-     */
-    public static function prepare()
-    {
-        static::startChromeDriver();
+  /**
+   * Prepare for Dusk test execution.
+   *
+   * @beforeClass
+   * @return void
+   */
+  public static function prepare()
+  {
+    if(env('APP_ENV', 'local')) {
+      static::startChromeDriver();
     }
+  }
 
-    /**
-     * Create the RemoteWebDriver instance.
-     *
-     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
-     */
-    protected function driver()
-    {
-        return RemoteWebDriver::create(
-            'http://localhost:9515', DesiredCapabilities::phantomjs()
-        );
-    }
+  /**
+   * Create the RemoteWebDriver instance.
+   *
+   * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+   */
+  protected function driver()
+  {
+    return RemoteWebDriver::create(
+        'http://localhost:9515', DesiredCapabilities::phantomjs()
+    );
+  }
 }
