e3e3d8f0e10c06213f0209602ea4d9e8a9ffb83b
Robert Hamel <roberthamel@gmail.com>
2017-05-11 14:34:11 -0500

    reworking docs; moved all npm dependencies to dev

--- log/log.md
+++
@@ -1,9 +0,0 @@
-# Log Entries
-
-## User Stories
-
-[User Stories](user-stories.md)
-
-## Week 1
-
-[Week 1 Log Entry](week1/log.md)
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .browserSync({ proxy: 'rdx.dev', notify: false })
+   .browserSync({ proxy: 'r-doc.dev', notify: false })
    .disableNotifications();
