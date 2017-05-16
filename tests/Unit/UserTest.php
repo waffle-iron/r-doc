ceb9ae0e72500b4b39498dfbbc66e4fe179a2316
Robert Hamel <roberthamel@gmail.com>
2017-05-16 13:04:59 -0500

    Refactor one to one relationship

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -14,10 +14,7 @@ class UserTest extends TestCase
   /** @test */
   public function a_user_has_an_image()
   {
-    $user = create('App\User', ['image_id' => null]);
-    $image = create('App\Image', ['user_id' => 1]);
-
-    $this->assertInstanceOf('App\Image', $user->image);
+    $this->oneToOne('User', 'Image');
   }
 
   /** @test */
--- tests/utilities/Relationships.php
+++ tests/utilities/Relationships.php
@@ -6,15 +6,36 @@ use function strtolower;
 
 trait Relationships
 {
+  public function oneToOne($first, $second)
+  {
+    $lowerFirst = $this->lower($first);
+    $lowerSecond = $this->lower($second);
+
+    $one = create("App\\$first", ["{$lowerSecond}_id" => null]);
+    $two = create("App\\$second", ["{$lowerFirst}_id" => 1]);
+
+    $this->assertInstanceOf("App\\$second", $one->$lowerSecond);
+  }
+
   public function manyToOne($many, $one)
   {
-    $lowerMany = strtolower($many);
-    $lowerOne = strtolower($one);
-    $pluralMany = "{$lowerMany}s";
+    $lowerMany = $this->lower($many);
+    $lowerOne = $this->lower($one);
+    $pluralMany = $this->plural($lowerMany);
 
     $first = create("App\\$one");
     $second = create("App\\$many", ["{$lowerOne}_id" => $first->id], 10);
 
     $this->assertCount(10, $first->$pluralMany);
+  }
+
+  private function lower($string)
+  {
+    return strtolower($string);
+  }
+
+  private function plural($string, $suffix = 's')
+  {
+    return "$string$suffix";
   }
 }
