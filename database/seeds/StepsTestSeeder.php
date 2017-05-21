599ddf68e0863bc010835c120ddc7433e935af76
Robert Hamel <roberthamel@gmail.com>
2017-05-21 16:29:50 -0500

    Create two new database test seeder files

---
+++ database/seeds/StepsTestSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\Image;
+use Illuminate\Database\Seeder;
+
+class StepsTestSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @param \Faker\Generator $faker
+   * @return void
+   */
+  public function run(Faker\Generator $faker)
+  {
+    $bullets = [
+        'black',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'light-blue',
+        'blue',
+        'violet',
+        'caution',
+        'info',
+        'reminder',
+    ];
+
+    $steps = factory(App\Step::class, $faker->numberBetween(7, 15))->create();
+    $steps->each(function ($s, $key) use ($faker, $bullets) {
+      $s->update(['orderby' => $key + 1]);
+      $images = factory(Image::class, $faker->numberBetween(2, 3))->create();
+      $images->each(function ($img, $key) use ($s) {
+        $img->update(['orderby' => $key + 1]);
+        $s->images()->save($img);
+      });
+      $guide = App\Guide::find(1);
+      $guide->steps()->save($s);
+      $lines = factory(App\Line::class, $faker->numberBetween(3, 8))->create()->each(function ($l, $key) use ($faker, $bullets) {
+        if ($key === 0) {
+          $l->update([
+              'bullet' => 'black',
+              'orderby' => $key + 1,
+              'level' => 0
+          ]);
+        } else {
+          $level = 1;
+          $key - 1 === 1 ? $level = $faker->numberBetween(0, 2) : $faker->numberBetween(1, 2);
+          $l->update([
+              'bullet' => $faker->randomElement($bullets),
+              'orderby' => $key + 1,
+              'level' => $level,
+          ]);
+        }
+      });
+      $s->lines()->saveMany($lines);
+      $revision = factory(App\Revision::class)->create([
+          'owner_id' => $faker->numberBetween(1, 50),
+      ]);
+      $status = App\Status::find(1);
+      $revision->status()->associate($status);
+      $revision->steps()->save($s);
+    });
+  }
+}
