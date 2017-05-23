4625b8cb7378e2090e8294cbc8bacbd6111d1925
Robert Hamel <roberthamel@gmail.com>
2017-05-23 15:19:23 -0500

    Install vuetify

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,14 +5,11 @@
  */
 
 require('./bootstrap');
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(Vuetify);
 
 // Root level components
 Vue.component('guide', require('./pages/Guide.vue'));
