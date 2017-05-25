74c9a383d40c27e5e8baf79c68b11766f462a0c4
Robert Hamel <roberthamel@gmail.com>
2017-05-24 21:46:06 -0500

    post fortrabbit deploy

--- fortrabbit-deploy.sh
+++ fortrabbit-deploy.sh
@@ -2,11 +2,11 @@
 
 mv .gitignore local-gitignore
 mv fortrabbit-gitignore .gitignore
-git add .
+git add --all
 git commit -m "deploy to fortrabbit"
 git push fortrabbit master --no-verify
 mv .gitignore fortrabbit-gitignore
 mv local-gitignore .gitignore
-git add .
+git add --all
 git commit -m "post fortrabbit deploy"
 git push origin master
