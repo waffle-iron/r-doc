3f72f4561a940005c45959e19940b4585763e499
Robert Hamel <roberthamel@gmail.com>
2017-05-25 15:34:12 -0500

    Begin autocomplete implement

--- resources/assets/js/components/GuideSearch.vue
+++ resources/assets/js/components/GuideSearch.vue
@@ -2,16 +2,20 @@
   <div id="guide-search">
     <v-text-field name="search-input"
                   label="Search documentation"
-                  id="search-documentation">
+                  id="search-documentation-temp">
     </v-text-field>
   </div>
 </template>
 
 <script>
   export default {
+    created() {
+      axios.get('/api/v1/guides/search')
+          .then(r => this.results = r.data)
+    },
     data() {
       return {
-        
+        results: [],
       }
     }
   }
