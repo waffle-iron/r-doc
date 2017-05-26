615c1c532f487f7c66304161813834671b7ac28e
Robert Hamel <roberthamel@gmail.com>
2017-05-26 12:57:11 -0500

    Modify deploy script

--- fortrabbit-deploy.sh
+++ fortrabbit-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+npm run production
 mv .gitignore local-gitignore
 mv fortrabbit-gitignore .gitignore
 git add --all
@@ -7,6 +8,7 @@ git commit -m "deploy to fortrabbit"
 git push fortrabbit master --no-verify
 mv .gitignore fortrabbit-gitignore
 mv local-gitignore .gitignore
+npm run dev
 git add --all
 git commit -m "post fortrabbit deploy"
 git push origin master
