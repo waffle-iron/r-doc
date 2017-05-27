983c7ac236db71230bcf1c1b5f409c500f3c8817
Robert Hamel <roberthamel@gmail.com>
2017-05-27 15:02:48 -0500

    deploy to fortrabbit

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -1,6 +1,5 @@
-export const API_DOMAIN = 'http://r-doc.dev';
-export const LOGIN_URL = API_DOMAIN + '/oauth/token';
-export const USER_URL = API_DOMAIN + '/api/user';
+export const LOGIN_URL = '/oauth/token';
+export const USER_URL = '/api/user';
 
 export const getHeader = function() {
   const tokenData = JSON.parse(window.localStorage.getItem('authUser'));
