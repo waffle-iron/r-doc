3f9d4e349b0acf580a88960ddee8ad3c9fdd8fe4
Robert Hamel <roberthamel@gmail.com>
2017-05-27 15:06:30 -0500

    deploy to fortrabbit

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -1,5 +1,6 @@
-export const LOGIN_URL = '/oauth/token';
-export const USER_URL = '/api/user';
+export const API_DOMAIN = location.origin;
+export const LOGIN_URL = API_DOMAIN + '/oauth/token';
+export const USER_URL = API_DOMAIN + '/api/user';
 
 export const getHeader = function() {
   const tokenData = JSON.parse(window.localStorage.getItem('authUser'));
